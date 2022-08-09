a40c5f48b62292aa921908c0978ba0778a9ef510
MariannaPo <mapodmasko@gmail.com>
2022-08-09 16:21:11 +0300

    after 12 lessons

---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://c4.wallpaperflare.com/wallpaper/414/412/906/moscow-night-city-wallpaper-preview.jpg"
+                    alt="profile panorama"/>
+            </div>
+            <div>
+                <div>
+                    ava + descriptions
+                </div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
