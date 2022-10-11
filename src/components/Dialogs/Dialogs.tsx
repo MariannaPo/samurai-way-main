67fc3b6376ee7bfd658ac91873fb42101a3da884
MariannaPo <mapodmasko@gmail.com>
2022-10-11 16:02:03 +0300

    after 24 lessons

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,12 +4,12 @@ import {NavLink} from "react-router-dom";
 
 type DialogsItemType = {
     name: string
-    id: string
+    id: number
 }
 
 const DialogItem = (props: DialogsItemType) => {
     let path = "/dialogs/" + props.id;
-    return  <div className={s.dialog + ' ' + s.active}>
+    return <div className={s.dialog + ' ' + s.active}>
         <NavLink to={path}>{props.name}</NavLink>
     </div>
 }
@@ -22,24 +22,44 @@ const Message = (props: MessageType) => {
 }
 
 const Dialogs = (props: DialogsItemType) => {
+
+    let dialogsData = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Andrey"},
+        {id: 3, name: "Sveta"},
+        {id: 4, name: "Sasha"},
+        {id: 5, name: "Viktor"},
+        {id: 6, name: "Valera"},
+    ]
+
+    let messageData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: `How is your it-kamasutra?`},
+        {id: 3, message: "Yo"},
+        {id: 4, message: "Yo"},
+        {id: 5, message: "Buy"},
+        {id: 6, message: "What?"},
+    ]
+
     return (
         <div className={s.dialogs}>
-                <div className={s.dialogsItems}>
-                    <DialogItem name="Dimych" id="1"/>
-                    <DialogItem name="Andrey" id="2"/>
-                    <DialogItem name="Sveta" id="3"/>
-                    <DialogItem name="Sasha" id="4"/>
-                    <DialogItem name="Viktor" id="5"/>
-                    <DialogItem name="Valera" id="6"/>
-                </div>
-                <div className={s.messages}>
-                    <Message message= 'Hi'/>
-                    <Message message= 'How is your it-kamasutra?'/>
-                    <Message message= 'Yo'/>
-                    <Message message= 'Yo'/>
-                    <Message message= 'Yo'/>
-                </div>
+            <div className={s.dialogsItems}>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
             </div>
+            <div className={s.messages}>
+                <Message message={messageData[0].message}/>
+                <Message message={messageData[1].message}/>
+                <Message message={messageData[2].message}/>
+                <Message message={messageData[3].message}/>
+                <Message message={messageData[4].message}/>
+                <Message message={messageData[5].message}/>
+            </div>
+        </div>
     )
 }
 export default Dialogs;
--- src/components/Profile/MyPosts/Posts/MyPosts.tsx
+++ src/components/Profile/MyPosts/Posts/MyPosts.tsx
@@ -3,7 +3,14 @@ import sta from "./MyPosts.module.css"
 import Post from "./Post";
 
 
+
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: 'Hi, how are u?', likesCount: 0},
+        {id: 2, message: `It's my first post`, likesCount: 23},
+    ]
+
     return (
         <div className={sta.postsBlock}>
             <h3>My posts</h3>
@@ -16,8 +23,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={sta.posts}>
-                <Post message='Hi, how are u?' likesCount='0'/>
-                <Post message={`It's my first post`} likesCount='23'/>
+                <Post id={postData[0].id} message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post id={postData[1].id} message={postData[1].message} likesCount={postData[1].likesCount}/>
             </div>
         </div>
 
